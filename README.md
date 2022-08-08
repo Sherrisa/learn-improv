c1777674b06a4f8d5e79ab43cea3d91e4f4e0667
Sherrisa Classon <sherrisaclasson@outlook.com>
2022-08-08 13:36:18 -0700

    JQueryUI Accordion, Chord API & Cycle 2

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-Here are some resources I referenced:
-https://gist.github.com/mindplace/b4b094157d7a3be6afd2c96370d39fad
-https://medium.com/@winnieliang/how-to-run-a-simple-html-css-javascript-application-on-heroku-4e664c541b0b
+How to Run a Simple HTML/CSS/Javascript Application on Heroku
+https://medium.com/@winnieliang/
+how-to-run-a-simple-html-css-javascript-application-on-heroku-4e664c541b0b
+
+Documentation for rapidapi.com Piano Chords API
+https://pianoapi.jsdisco.dev/
+
+This is where I got the jQuery UI code snippets for the link and script
+https://developers.google.com/speed/libraries/
+
+Documentation for JQuery UI: Accordion 
+https://jqueryui.com/accordion/
+
+Documentation for Cycle 2
+https://jquery.malsup.com/cycle2/api/
+
+How to use an unordered list of text for the slides in Cycle 2
+https://jquery.malsup.com/cycle2/faq/
+
+CSS variables review
+https://developer.mozilla.org/en-US/docs/Web/CSS/var
+https://www.youtube.com/watch?v=8Yum7EQgadM
--- improv.js
+++ improv.js
@@ -1,3 +1,7 @@
+"use strict";
+
+let favoriteFills = [];
+
 window.addEventListener("DOMContentLoaded", function () {
   document
     .querySelector("#showNotesBtn")
@@ -10,11 +14,16 @@ window.addEventListener("DOMContentLoaded", function () {
       const selectedChord =
         chordDropdown.options[chordDropdown.selectedIndex].value;
 
-      // Get and display chord
+      // get and display chord
       showNotes(selectedRoot, selectedChord);
     });
+    
+    favoriteFills = loadFills();
+    console.log("favoriteFills: " + favoriteFills);
+    document.getElementById("favoriteFills").innerHTML = favoriteFills;
 });
 
+// access data for Chord Speller for Lesson 1
 function showNotes(root, name) {
   // create variable named data per the rapidApi docs
   const data = null;
@@ -42,7 +51,6 @@ function showNotes(root, name) {
       console.log(response.error);
 
       let html = "";
-      html += "<p></p>";
       if (!response.error) {
         html += `${response.name} = `;
         for (let c = 0; c < response.notes.length; c++) {
@@ -66,3 +74,59 @@ function showNotes(root, name) {
   // send HTTP request
   xhr.send(data);
 }
+
+// accordion for the major, minor, diminished and augmented chords
+$(".accordion").accordion({
+  collapsible: true,
+  heightStyle: "content",
+});
+
+// JQuery & Cycle2 Slideshow
+let $slideshow = $(".cycle-slideshow");
+
+$slideshow.click(function () {
+  if ($slideshow.is(".cycle-paused")) $slideshow.cycle("resume");
+  else $slideshow.cycle("pause");
+});
+
+$slideshow.cycle({
+  fx: "fadeout",
+  speed: "200",
+  timeout: "3000",
+  pauseOnHover: "true",
+  autoHeight: "container",
+  slides: "li",
+});
+
+// for accessibility there must be a clear way to pause
+$("#pauseBtn").click(function () {
+  $slideshow.cycle("pause");
+});
+$("#resumeBtn").click(function () {
+  $slideshow.cycle("resume");
+});
+
+// on page load and display the favorite fills from localStorage
+function loadFills() {
+  if (localStorage.getItem("fills")) {
+    return localStorage.getItem("fills").split();
+  }else{
+    return [];
+  }
+}
+
+// save contents of textarea box to localStorage on click of save button
+$("#saveFills").click(function(){
+  // get the contents of the textarea input box
+  let fillsTextInput = document.querySelector("#favoriteFills");
+  let fills = fillsTextInput.value;
+
+  // save contents of textarea to localStorage
+  localStorage.setItem("fills", fills.toString());
+});
+
+$("#clearFills").click(function(){
+  localStorage.clear();
+  document.getElementById("favoriteFills").innerHTML = [];
+})
+
